28c6be4666dfbec5a3dfbf14ad138c535c8affff
definem <mmm748844@gmail.com>
2023-10-17 22:43:56 +0500

    intro section end

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,17 @@
 import {useEffect} from "react";
 import Image from "next/image";
 import About from "@/components/About/about";
+import Intro from "@/components/Intro/intro";
 
 const page = () => {
     return (
-        <>
-            {/* <div className="  border-blue-600 my-5 container mx-auto p-5 px-4 border">
+      <>
+        {/* <div className="  border-blue-600 my-5 container mx-auto p-5 px-4 border">
                 <h1>HOME PAGE</h1>
             </div> */}
-            <About/>
-        </>
+        <Intro />
+        <About />
+      </>
     );
 };
 
