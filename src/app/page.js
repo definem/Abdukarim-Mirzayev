7f17e446f0070bcd379f92f370eb007024e748ac
definem <mmm748844@gmail.com>
2023-10-17 10:23:58 +0500

    created

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+"use client";
+import {useEffect} from "react";
+import Image from "next/image";
+
+const page = () => {
+    return (
+        <>
+            <div className="  border-blue-600 my-5 container mx-auto p-5 px-4 border">
+                <h1>HOME PAGE</h1>
+            </div>
+        </>
+    );
+};
+
+export default page;
