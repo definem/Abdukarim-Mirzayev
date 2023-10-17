d2a268fa04a87302cd3c2690835910c881fcac85
definem <mmm748844@gmail.com>
2023-10-17 22:41:44 +0500

    intro section end

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BackgroundImg from "../../../public/assets/images/user.png";
+import Arrow from "../../../public/assets/icons/arrow.svg";
 
 const About = () => {
   return (
@@ -18,15 +19,19 @@ const About = () => {
             style={{ paddingTop: "324px" }}
             className="text-[64px] text-white text-bold max-w-[580px]"
           >
-            <h1 className="font-semibold px-6 w-[790px]">
+            <h1 className="font-semibold px-6 w-[790px] mb-[20px]">
               Abdukarim Mirzayev bilan{" "}
               <span className="text-[#00E689]">storytelling</span> kursi...
             </h1>
-            <h4 className="text-[16px] text-white font-normal w-[430px] px-6">
+            <p className="text-[16px] text-white font-normal w-[430px] px-6">
               Ushbu kurs yordamida jozibali gapirish, sahnada va kamera
               qarshisida hayajonlanmaslik, vizual kompozitsiya va ovozlashtirish
               sirlarini bilib olasiz.
-            </h4>
+            </p>
+            <button className="flex items-center py-[25px] rounded-[3px] mt-[40px] px-[14px] gap-[5px] bg-[#FAA40C] hover:bg-orange-400 text-[16px] mx-6 w-[207px] h-[48px] text-black font-semibold">
+              Guruhga qo’shilish
+              <img src={Arrow.src} alt="arrow icon" />
+            </button>
           </div>
         </div>
       </div>
